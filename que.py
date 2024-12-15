afaffc07076b33b237c1011b14bd439f289ef2ba
PruneKushagraRathi <116707416+PruneKushagraRathi@users.noreply.github.com>
2024-12-15 16:14:27 +0530

    stock

--- que.py
+++ que.py
@@ -252,3 +252,19 @@ def stock_span(prices):
 
 prices = [100, 80, 60, 70, 60, 75, 85]
 print(stock_span(prices))  # Output: [1, 1, 1, 2, 1, 4, 6]
+#next greater element
+def next_greater_element(arr):
+    stack = []
+    result = [-1] * len(arr)
+
+    for i in range(len(arr) - 1, -1, -1):
+        while stack and stack[-1] <= arr[i]:
+            stack.pop()
+        if stack:
+            result[i] = stack[-1]
+        stack.append(arr[i])
+
+    return result
+
+arr = [4, 5, 2, 10, 8]
+print(next_greater_element(arr))  # Output: [5, 10, 10, -1, -1]
