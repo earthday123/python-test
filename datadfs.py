89a794f54f09f2edaf52ff01447ce52bad2b033b
PruneKushagraRathi <116707416+PruneKushagraRathi@users.noreply.github.com>
2024-12-15 16:24:03 +0530

    dfs iterative function

--- datadfs.py
+++ datadfs.py
@@ -19,3 +19,29 @@ def dfs_recursive(node, visited):
 # Usage
 visited = set()
 dfs_recursive('A', visited)
+#DFS Using a Stack (Iterative)
+# Graph represented as an adjacency list
+graph = {
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F'],
+    'D': [],
+    'E': ['F'],
+    'F': []
+}
+
+# DFS function
+def dfs_iterative(start):
+    visited = set()
+    stack = [start]
+    
+    while stack:
+        node = stack.pop()
+        if node not in visited:
+            print(node, end=" ")  # Process the node
+            visited.add(node)
+            stack.extend(reversed(graph[node]))  # Add neighbors to the stack
+
+# Usage
+dfs_iterative('A')
+
